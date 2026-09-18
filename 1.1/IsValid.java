d9f360c218a6a17889683b8543e03d6638ab0cec
qqwl2 <qqwl2@mail.ru>
2026-09-18 14:52:52 +0600

    changed for stack

--- 1.1/IsValid.java
+++ 1.1/IsValid.java
@@ -1,18 +1,25 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
-public class IsValid {                        
-    public static boolean isValid(String s) {   
-        Deque<Character> stack = new ArrayDeque<>();
+public class IsValid {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
+            if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
             } else {
-                if (stack.isEmpty()) return false;
-                char open = stack.pop();
-                if (c == ')' && open != '(') return false;
-                if (c == '}' && open != '{') return false;
-                if (c == ']' && open != '[') return false;
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+                if (c == ']' && top != '[') {
+                    return false;
+                }
             }
         }
         return stack.isEmpty();
